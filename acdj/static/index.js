ef501e3badcdeae63964f9731ea76318fdb68130
Amaro Ferraris <amaroferraris@gmail.com>
2024-05-29 23:34:48 -0300

    HTML, CSS, JS adding btnPresupuesto

--- acdj/static/index.js
+++ acdj/static/index.js
@@ -12,6 +12,7 @@ let jsonCarModel = JSON.parse(document.getElementById('carmodel-json').textConte
 let jsonCarAccessory = JSON.parse(document.getElementById('caraccessory-json').textContent)
 
 const btn = document.getElementById('btn-output');
+const btnPresupuesto = document.getElementById('btn-output-presupuesto');
 const message = document.getElementById('output');
 
 
@@ -245,7 +246,7 @@ btn.addEventListener('click', () => {
 \nEl monto de tu orden se congela por 48hs hábiles y podés abonar hasta el ${formattedFutureDate} inclusive.`;
 
     // CHECKING IF A COPY BUTTON ALREADY EXISTS
-    const btnGenerateNextElement = btn.nextElementSibling;
+    const btnGenerateNextElement = btn.nextElementSibling.nextElementSibling;
     const messagePreviousElement = message.previousElementSibling;
 
     if (btnGenerateNextElement
@@ -259,7 +260,7 @@ btn.addEventListener('click', () => {
     };
 
     // CREATING A COPY BUTTON
-    const btnCopyCreate = '<button class="btn-copy" id="btn-copy">COPY MESSAGE</button>';
+    const btnCopyCreate = '<button class="btn-copy" id="btn-copy">COPIAR</button>';
     message.insertAdjacentHTML('beforebegin', btnCopyCreate)
 
 
@@ -285,7 +286,118 @@ btn.addEventListener('click', () => {
     });
 
     // CREATING A CLEAR MESSAGE BUTTON
-    const btnClearCreate = '<button class="btn-clear" id="btn-clear">CLEAR MESSAGE</button>';
+    const btnClearCreate = '<button class="btn-clear" id="btn-clear">BORRAR</button>';
+    message.insertAdjacentHTML('beforebegin', btnClearCreate);
+
+    const btnClear = document.getElementById('btn-clear')
+
+    btnClear.addEventListener('click', () => { message.innerHTML = '' });
+});
+
+
+// PRESUPUESTO 
+btnPresupuesto.addEventListener('click', () => {
+
+    // FILTERING CHECKED BOXES
+    // Getting model id
+    const checkboxModel = document.querySelectorAll('input[name="model"]')
+    let checkedModelId;
+
+    checkboxModel.forEach(checkbox => {
+        if (checkbox.checked) {
+            checkedModelId = checkbox.id.split('-')[1]
+        }
+    })
+
+    // Getting accessory
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+    const checkedAccesoriesName = [];
+    const checkedAccesoriesPrice = [];
+
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+
+            checkedAccesoriesName.push(checkbox.value.split(' (EXCEPTO')[0]);
+            checkedAccesoriesPrice.push(jsonCarAccessory.filter(item => item.id == checkbox.id.split('-')[1] && item.model_id == checkedModelId)[0].price)
+
+        }
+    })
+
+
+    let accesoryNameAndPrice = ''
+    for (const item in checkedAccesoriesName) {
+        accesoryNameAndPrice += `<li> - ${checkedAccesoriesName[item]}: $${checkedAccesoriesPrice[item].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }).replace('R$', '')}\n</li>`
+    }
+
+
+    // // GETTING DATE
+    // const currentDate = new Date();
+
+    // // Add 48 hours to the current date
+    // const futureDate = new Date(currentDate.getTime() + (48 * 60 * 60 * 1000));
+
+    // // Check if the future date falls on a Saturday or Monday
+    // if (futureDate.getDay() === 6) { // Saturday
+    //     futureDate.setDate(futureDate.getDate() + 2); // Add 2 days to skip Saturday
+    // } else if (futureDate.getDay() === 0) { // Monday
+    //     futureDate.setDate(futureDate.getDate() + 1); // Add 1 day to skip Monday
+    // }
+
+    // const days = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+    // const dayName = days[futureDate.getDay()];
+    // const dayNumber = futureDate.getDate();
+    // const month = futureDate.getMonth() + 1; // Note: January is month 0, so we add 1
+
+    // const formattedFutureDate = `${dayName} ${dayNumber < 10 ? '0' + dayNumber : dayNumber}/${month < 10 ? '0' + month : month}`;
+
+    message.innerHTML = '';
+    message.innerHTML = `Te paso el presupuesto de los accesorios de tu interés:\n
+<ul>${accesoryNameAndPrice}</ul>
+- Total: $${checkedAccesoriesPrice.reduce((acc, currVal) => acc + currVal, 0).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }).replace('R$', '')}
+\nEl presupuesto tiene validez por 48hs hábiles, pasado este plazo los valores quedan sujetos a modificación hasta el momento de su confirmación.`;
+
+    // CHECKING IF A COPY BUTTON ALREADY EXISTS
+    const btnGenerateNextElement = btnPresupuesto.nextElementSibling;
+    const messagePreviousElement = message.previousElementSibling;
+
+    if (btnGenerateNextElement
+        && btnGenerateNextElement.className === 'btn-copy'
+        || btnGenerateNextElement.className === 'btn-copied'
+        && messagePreviousElement.className === 'btn-clear'
+    ) {
+        // IF IT DOES, REMOVE IT (TO AVOID MULTIPLE COPY BUTTONS)
+        btnGenerateNextElement.remove();
+        messagePreviousElement.remove();
+    };
+
+    // CREATING A COPY BUTTON
+    const btnCopyCreate = '<button class="btn-copy" id="btn-copy">COPIAR</button>';
+    message.insertAdjacentHTML('beforebegin', btnCopyCreate)
+
+
+    const btnCopy = document.getElementById('btn-copy')
+
+    btnCopy.addEventListener('click', () => {
+
+        const copyText = document.getElementById('output');
+        const copiedText = copyText.textContent;
+
+        navigator.clipboard.writeText(copiedText)
+            .then(() => {
+                btnCopy.classList.remove('btn-copy');
+                btnCopy.classList.add('btn-copied');
+                setTimeout(() => {
+                    btnCopy.classList.remove('btn-copied');
+                    btnCopy.classList.add('btn-copy');
+                }, 500)
+            })
+            .catch(err => {
+                console.error('Error copying message to clipboard: ', err);
+            });
+    });
+
+    // CREATING A CLEAR MESSAGE BUTTON
+    const btnClearCreate = '<button class="btn-clear" id="btn-clear">BORRAR</button>';
     message.insertAdjacentHTML('beforebegin', btnClearCreate);
 
     const btnClear = document.getElementById('btn-clear')
